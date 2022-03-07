b383d890c660f1f3246bd3d5575799dc9a80e0be
abdullah <abdullah.alabdullatif.19@ucl.ac.uk>
2022-03-07 02:25:39 +0000

    tested new models

--- venv/simple_test.py
+++ venv/simple_test.py
@@ -7,13 +7,13 @@ import os.path
 from tensorflow.keras.models import Model, load_model
 import time
 
-env = football_env.create_environment(env_name='academy_empty_goal', representation='simple115v2', render=True)
+env = football_env.create_environment(env_name='academy_empty_goal', representation='simple115v2', render=True, rewards='scoring')
 
 n_actions = env.action_space.n
 dummy_n = np.zeros((1, 1, n_actions))
 dummy_1 = np.zeros((1, 1, 1))
 
-model_actor = load_model('models/Empty_Goal/model_actor_780_0.7000000104308128.hdf5', custom_objects={'loss': 'categorical_hinge'})
+model_actor = load_model('models/Empty_Goal_noCheckpoint/model_actor_400_0.0.hdf5', custom_objects={'loss': 'categorical_hinge'})
 
 state = env.reset()
 done = False
@@ -22,14 +22,14 @@ restart_counter = 0
 while True:
     state_input = K.expand_dims(state, 0)
     s1 = time.time()
-    action_dist_tensor = model_actor([state_input, dummy_n, dummy_1, dummy_1, dummy_1]) # uses the actor model to predict the best actions
-  #  action_dist = model_actor.predict([state_input])
-    action_dist = action_dist_tensor.numpy()
+    #action_dist = model_actor.predict([state_input, dummy_n, dummy_1, dummy_1, dummy_1], steps=1) # uses the actor model to predict the best actions
+    action_dist = model_actor([state_input, dummy_n, dummy_1, dummy_1, dummy_1]).numpy()
+    #action_dist = action_dist_tensor.numpy()
     #print("predict action: " + str(time.time() - s1))
     action = np.random.choice(n_actions, p=action_dist[0, :])
     next_state, reward, done, _ = env.step(action)
     total_rewards += reward
-    print('action=' + str(action) + ', reward=' + str(reward) + ', total goals =' + str(total_rewards) + ', probs =' + str(action_dist))
+    print('action=' + str(action) + ', reward=' + str(reward) + ', total rewards =' + str(total_rewards) + ', probs =' + str(action_dist))
     state = next_state
     if done:
         state = env.reset()
