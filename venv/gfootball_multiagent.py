ce5262bec637db7b2ee9dfe2321f9d61e3943a0f
Abdullah Alabdullatif <abdullah.alabdullatif.19@ucl.ac.uk>
2022-03-29 14:20:17 +0100

    Update gfootball_multiagent.py

--- venv/gfootball_multiagent.py
+++ venv/gfootball_multiagent.py
@@ -74,7 +74,7 @@ class RllibGFootball(MultiAgentEnv):
 
 if __name__ == '__main__':
   args = parser.parse_args()
-  ray.init()
+  ray.init(num_gpus=1)
 
   # Simple environment with `num_agents` independent players
   register_env('gfootball', lambda _: RllibGFootball(args.num_agents))
@@ -111,7 +111,7 @@ if __name__ == '__main__':
           'batch_mode': 'truncate_episodes',
           'observation_filter': 'NoFilter',
           'vf_share_layers': 'true',
-          #'num_gpus': 1,
+          'num_gpus': 1,
           'lr': 2.5e-4,
           'log_level': 'DEBUG',
           'simple_optimizer': args.simple,
